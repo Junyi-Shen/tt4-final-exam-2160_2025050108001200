194a144be76243a9ce156dcbb02cd571090615a5
Junyi Shen <wakaba04159@gmail.com>
2025-05-01 10:39:03 -0400

    created backend and frontend

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ backend/Models/Movie.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace backend.Models
+{
+    public class Movie
+    {
+        public int ID {get; set;}
+        public string Title {get; set;}
+        public string Genre {get; set;}
+        public bool Watched {get; set;}
+        public double Rating {get; set;}
+    }
+}
